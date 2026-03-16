f2b9cc8c37d66d05dda161cfdca866fe8cd08dfe
Erick Lima <ericklima3784@gmail.com>
2026-03-16 14:39:18 -0300

    Init

---
+++ script.js
@@ -0,0 +1,29 @@
+const DOMIdade = document.querySelectorAll(".idade");
+const mobileMenu = document.getElementById("mobile-menu-items");
+
+DOMIdade.forEach((element) => {
+    element.append(calcularIdade() + " anos");
+})
+
+function calcularIdade() {
+    let hoje = new Date();
+    let nascimento = new Date(2003, 11, 6);
+
+    return transformarMilisegundosEmAnos(hoje - nascimento);
+}
+
+function transformarMilisegundosEmAnos(milisegundos) {
+    return Math.trunc((milisegundos / (1000 * 60 * 60 * 24)) / 365);
+}
+
+function toggleMobileMenu() {
+    console.log("oi")
+    mobileMenu.style.display = mobileMenu.style.display == "flex" ? "none" : "flex";
+    console.log(mobileMenu.style.display)
+}
+
+document.addEventListener("click", (event) => {
+    if (mobileMenu.style.display == "flex") {
+        event.target.id != "button-menu" && toggleMobileMenu()
+    }
+});
